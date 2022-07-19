656863b779a2b77b62b97afd96407b8507726d51
paolchol <paolocolombo1996@gmail.com>
2022-07-19 23:45:51 +0200

    SGI visualization

---
+++ analisi_serie_storiche/SGI.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+SGI computation on the basin's head time series
+
+@author: paolo
+"""
+
+# %% Setup
+
+import pastas as ps
+import pandas as pd
+import numpy as np
+import dataanalysis as da
+import dataviz as dv
+import matplotlib.pyplot as plt
+
+# %% Load data and metadata
+
+head = pd.read_csv('data/head_IT03GWBISSAPTA.csv', index_col = 'DATA')
+meta = pd.read_csv('data/metadata_piezometri_ISS.csv')
+
+# %% Select solid time series and fill missing values
+
+cn = da.CheckNA(head)
+filtered, removed = cn.filter_col(20, True)
+head_fill = filtered.interpolate('linear', limit = 12)
+
+# %% Compute the SGI for each well/piezometer
+
+head_fill.index = pd.DatetimeIndex(head_fill.index)
+sgi_db = head_fill.copy()
+for col in head_fill.columns:
+    idx = np.invert(head_fill[col].isna())
+    sgi_db.loc[idx, col] = ps.stats.sgi(head_fill[col].dropna())
+
+# sgi_db.to_csv('analisi_serie_storiche/SGI_db.csv')
+
+# %% Visualize the SGI
+
+#Single SGI plot
+dv.plot_SGI(sgi_db.iloc[:, 4])
+#Plot all
+for col in sgi_db.columns:
+    dv.plot_SGI(sgi_db[col])
+
+#Heatmap visualization of the whole SGI db
+col_labels = sgi_db.resample('YS').mean().index.year
+fig, ax = plt.subplots(figsize = (6.4, 3.6), dpi = 500)
+im, cbar = dv.heatmap_TS(sgi_db.to_numpy().transpose().copy(),
+                   row_labels = sgi_db.columns, col_labels = col_labels,
+                   step = 12, ax = ax, cbarlabel = "SGI",
+                   rotate = True, aspect = 'auto',
+                   cmap = plt.get_cmap("coolwarm_r", 8))
+fig.tight_layout()
+plt.show()
+
+# %% Trial: yearly SGI
+
+# Yearly mean of SGI
+sgi_y = sgi_db.resample('YS').mean()
+#It doesn't really mean anything
--- dataviz.py
+++ dataviz.py
@@ -8,6 +8,8 @@ author: paolo
 from matplotlib import pyplot
 import plotly.express as px
 from plotly.offline import plot
+import matplotlib.pyplot as plt
+import numpy as np
 
 def fast_TS_visualization(df):
     pyplot.figure()
@@ -28,3 +30,71 @@ def interactive_TS_visualization(df, xlab = 'X', ylab = 'Y', file = 'temp.html')
 
 def fast_boxplot(df):
     df.boxplot()
+
+def plot_SGI(series, figsize = (6.4, 3.6), dpi = 500, dropna = True):    
+    sgi = series.dropna() if dropna else series
+    fig, ax = plt.subplots(figsize = figsize, dpi = dpi)
+    sgi.plot(color = "k")
+    ax.axhline(0, linestyle="--", color = "k")
+    droughts = sgi.to_numpy(copy = True)
+    droughts[droughts > 0] = 0
+    ax.fill_between(sgi.index, 0, droughts, color = "C0")
+    ax.set_title(series.name)
+
+def heatmap_TS(data, row_labels, col_labels, step, ax = None,
+            cbar_kw = {}, cbarlabel = "", white = False, rotate = False,
+            labelsize = 8, **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (M, N).
+    row_labels
+        A list or array of length M with the labels for the rows.
+    col_labels
+        A list or array of length N with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+    
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax = ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va = "bottom")
+
+    # Show all ticks and label them with the respective list entries.
+    ax.set_xticks(np.arange(0, data.shape[1], step), labels = col_labels)
+    ax.set_yticks(np.arange(data.shape[0]), labels = row_labels)
+    ax.tick_params(labelsize = labelsize)
+    # Let the horizontal axes labeling appear on top.
+    # ax.tick_params(top = True, bottom = False,
+    #                labeltop = True, labelbottom = False)
+
+    # Rotate the tick labels and set their alignment.
+    if rotate:
+        plt.setp(ax.get_xticklabels(), rotation = 30, ha = "right",
+        rotation_mode = "anchor")
+
+    # Turn spines off and create white grid.
+    if white:
+        ax.spines[:].set_visible(False)    
+        ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+        ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+        ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+        ax.tick_params(which="minor", bottom=False, left=False)
+    
+    return im, cbar
