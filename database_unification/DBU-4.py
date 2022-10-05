3984762a61256033ade7849fdc9801a6fbe5f111
paolchol <paolocolombo1996@gmail.com>
2022-10-05 15:41:58 +0200

    milano1950 database structure definition

--- database_unification/DBU-4.py
+++ database_unification/DBU-4.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 DBU: DataBase Unification
-Unificazione del dataset Olona con il database unificato DBU-3
+Unificazione del dataset Olona con il database unificato DBU-4
 
 Le operazioni necessarie per ottenere meta e head sono state svolte in
 Olona_struct.py
