7cd5a9ee2027703b88963b36c1c308095286abf3
paolchol <paolocolombo1996@gmail.com>
2022-10-10 18:18:21 +0200

    visualization of the metadata in IT03GWBISSAPTA

---
+++ trash/map_viz_trials.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Visualization trials of the dataset
+
+@author: paolo
+"""
+
+
+import geodata as gd
+import numpy as np
+import pandas as pd
+
+
+metaDBU = pd.read_csv('data/results/db-unification/meta_DBU-5.csv', index_col = 'CODICE')
+metaDBU['CODICE_SIF'] = [f"0{int(idx)}" if not np.isnan(idx) else np.nan for idx in metaDBU['CODICE_SIF']]
+
+viz = metaDBU.loc[metaDBU['BACINO_WISE'] == 'IT03GWBISSAPTA', :].copy().reset_index()
+viz.loc[viz['ORIGINE'].isna(), 'ORIGINE'] = 'PTUA2022'
+
+gd.show_mappoints(viz, 'lat', 'lon', file = 'trash/viz_trial.html', color = 'ORIGINE', hover_name = 'CODICE')
+
+
+#calcolare numero e percentuale di serie storiche aggiunte
