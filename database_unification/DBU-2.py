d8f38646cff40657ca81a865ef72f8d5bb29282a
paolchol <paolocolombo1996@gmail.com>
2022-10-12 13:14:34 +0200

    DBU-2 update

--- database_unification/DBU-2.py
+++ database_unification/DBU-2.py
@@ -10,6 +10,7 @@ CAP_struct.py
 
 # %% Setup
 
+import dataannalysis as da
 import datawrangling as dw
 import geodata as gd
 import pandas as pd
@@ -18,6 +19,8 @@ import numpy as np
 # %% Load CAP meta and data
 
 meta = pd.read_csv('data/CAP/meta_CAP.csv', index_col = 'CODICE')
+meta.index = [f"0{int(idx)}" if not np.isnan(idx) else np.nan for idx in meta.index]
+meta.index.names = ['CODICE']
 head = pd.read_csv('data/CAP/head_CAP.csv', index_col = 'DATA')
 head.index = pd.DatetimeIndex(head.index)
 
@@ -63,7 +66,6 @@ metamerge.drop(columns = ['COMUNE_CAP', 'X', 'Y', 'lat_CAP', 'lon_CAP'], inplace
 metamerge.insert(23, 'z_CAP', metamerge['Z'])
 metamerge.drop(columns = 'Z', inplace = True)
 
-# metamerge['CODICE_SIF'] = [f"0{int(idx)}" if not np.isnan(idx) else np.nan for idx in metamerge['CODICE_SIF']]
 metamerge.rename(columns = {'CODICE_SIF': 'CODICE_SIF_keep', 'index': 'CODICE_SIF_remove'}, inplace = True)
 metamerge = dw.joincolumns(metamerge, '_keep', '_remove')
 metamerge = dw.join_twocols(metamerge, ['ORIGINE', 'ORIGINE_CAP'], add = True, onlyna = False)
@@ -81,4 +83,47 @@ codes = metamerge.loc[idx, 'CODICE_SIF']
 #no need to perform a merge in this condition
 #head_DBU-1 is just saved as head_DBU-2
 
-headDBU.to_csv('data/results/db-unification/head_DBU-2.csv')
+# headDBU.to_csv('data/results/db-unification/head_DBU-2.csv')
+
+# %% Analyse the metadata not merged, identify points worth to be merged
+
+leftout = meta.loc[np.invert(meta.index.isin(metamerge['CODICE_SIF'])), :].copy()
+import dataviz as dv
+dv.interactive_TS_visualization(head[leftout.index], markers = True, file = 'plot/dbu/exploratory_DBU-2_CAPleftout.html')
+
+#identify the time series worth adding to DBU
+#visualize series which respect some conditions
+insert = da.stend_ts(leftout, head, '01-01-1990', '01-01-2010')
+dv.interactive_TS_visualization(head[insert], markers = True,
+                                file = 'plot/dbu/exploratory_DBU-2_CAPselection_1990.html',
+                                title = 'Time series starting before 1990 and ending after 2010')
+
+insert = da.stend_ts(leftout, head, delta = 20*365)
+dv.interactive_TS_visualization(head[insert], markers = True,
+                                file = 'plot/dbu/exploratory_DBU-2_CAPselection_20yrs.html',
+                                title = 'Time series with at least 20 years')
+#insert in DBU the ones selected by placing delta = 20
+
+# %% Update metamerge
+
+#export meta
+#join in QGIS with the basin
+#load
+# insert needed informations
+#join with metamerge
+
+# %% Merge the time series
+
+#aggiornare la sezione time series precedente pulendola
+#aggiungere le serie dati che ricadono in ISSAPTA
+
+headDBU = pd.read_csv('data/results/db-unification/head_DBU-1.csv', index_col='DATA')
+headDBU.index = pd.DatetimeIndex(headDBU.index)
+
+idx = (metamerge['CODICE_SIF'].isin(meta.index)) & (metamerge['BACINO_WISE'] == 'IT03GWBISSAPTA')
+codes = metamerge.loc[idx, 'CODICE_SIF']
+#no codes from CAP are present in IT03GWBISSAPTA
+#no need to perform a merge in this condition
+#head_DBU-1 is just saved as head_DBU-2
+
+# headDBU.to_csv('data/results/db-unification/head_DBU-2.csv')
